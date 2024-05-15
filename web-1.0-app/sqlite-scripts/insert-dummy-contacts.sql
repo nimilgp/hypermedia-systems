29b82c45693284b2f60a246dcaf794587ddb8e70
nimilgp <nimilgeorgepradeepngp@gmail.com>
2024-05-15 11:04:42 +0530

    sqlscript for inserting dummy-contacts

---
+++ web-1.0-app/sqlite-scripts/insert-dummy-contacts.sql
@@ -0,0 +1,4 @@
+INSERT INTO contacts (first_name, last_name, phone, email) VALUES	
+		('dio','brando', 9999999999, 'dio@jojo.com'),
+		('edward','elric',1234567890, 'edward@fma.com'),
+		('xin','li', 0000000221, 'xin@kingdom');
